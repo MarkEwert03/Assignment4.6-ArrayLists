def92ef8ec0c991bc4ce11746bda451989aa39e7
Mark Ewert <mewert03@gmail.com>
2021-01-12 12:27:00 -0800

    Initial sharing of project

---
+++ Question2.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+class Question2 {
+    
+    public static void removeSmallest(ArrayList<Integer> nums) {
+        //This method removes the smallest number in the list nums
+        int min = nums.get(0);
+        for (int i = 0; i < nums.size(); i++){
+            min = Math.min(min, nums.get(i));
+        }
+        for (int i = nums.size()-1; i >= 0; i--){
+            if (nums.get(i) == min) nums.remove(i);
+        }
+    }
+  
+    public static void main (String[] args) {
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            nums.add( (int) (Math.random()*100) );
+        }
+        
+        System.out.println("Our list before: " + nums);
+        removeSmallest(nums); 
+        System.out.println("Our list after:  " + nums);
+    }
+    
+    
+}
